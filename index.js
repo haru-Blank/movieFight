bb865d449ba272d5ad3726bf27edb04ac72311a4
harendraBhujel <avash.nekros274@gmail.com>
2020-04-01 14:46:29 +0545

    a follow up request has been implemented

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ const onInput = async event => {
       option.addEventListener('click', () => {
          dropdown.classList.remove('is-active');
          input.value = movie.Title;
+         onMovieSelect(movie);
       });
       resultsWrapper.appendChild(option);
    }
@@ -67,3 +68,13 @@ document.addEventListener('click', event => {
       dropdown.classList.remove('is-active');
    }
 });
+
+const onMovieSelect = async movie => {
+   const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+         apikey: 'b1b9a789',
+         i: movie.imdbID
+      }
+   });
+   console.log(response.data);
+};
