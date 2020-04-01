1a5c5095587e699d73113a281586221b3e6cf9ef
harendraBhujel <avash.nekros274@gmail.com>
2020-04-01 15:51:44 +0545

    create a autocompletee.js | made autocomplete totally usable

--- Note.md
+++ Note.md
@@ -10,3 +10,11 @@
 - **This app would be a lot better with some styling + CSS**
   *The starter kit we just set up has a CSS a framework called ‘Bulma CSS’ already wired up*
 
+## Issues with Implementation
+
+- All code touches everything
+- Autocomplete widget was supposed to be reusable
+- Autocomplete has knowledge of what a movie object is 
+- Autocomplete has knowledge of what to show for each option
+- Autocomplete has knowledge of what to do when a movie is clicked
+- Many global variables that refer to specific elements - it will be really hard to show a second autocomplete on the screen
--- index.js
+++ index.js
@@ -14,59 +14,16 @@ const fetchData = async searchTerm => {
    return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-   <label>
-      <b>Search for a Movie</b>
-   </label>
+createAutoComplete({
+   root: document.querySelector('.autocomplete')
+});
 
-   <input class="input" placeholder="Enter a movie Name">
+createAutoComplete({
+   root: document.querySelector('.autocomplete-two')
+});
 
-   <div class="dropdown">
-      <div class="dropdown-menu">
-         <div class="dropdown-content results"></div>
-      </div>
-   </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-   const movies = await fetchData(event.target.value);
-
-   if (!movies.length) {
-      dropdown.classList.remove('is-active');
-      return;
-   }
-
-   resultsWrapper.innerHTML = '';
-   dropdown.classList.add('is-active');
-
-   for (let movie of movies) {
-      const option = document.createElement('a');
-      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-      option.classList.add('dropdown-item');
-      option.innerHTML = `
-         <img src="${imgSrc}" alt="${movie.Title}" title="${movie.Title}"/>
-         ${movie.Title}
-      `;
-      option.addEventListener('click', () => {
-         dropdown.classList.remove('is-active');
-         input.value = movie.Title;
-         onMovieSelect(movie);
-      });
-      resultsWrapper.appendChild(option);
-   }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', event => {
-   if (!root.contains(event.target)) {
-      dropdown.classList.remove('is-active');
-   }
+createAutoComplete({
+   root: document.querySelector('.autocomplete-three')
 });
 
 const onMovieSelect = async movie => {
