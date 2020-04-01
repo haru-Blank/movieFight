bb1763b1bd4d36865c715d3fe4c5e0bca6f3400f
harendraBhujel <avash.nekros274@gmail.com>
2020-04-01 14:36:15 +0545

    dropdown close implemented

--- index.js
+++ index.js
@@ -36,13 +36,15 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
    const movies = await fetchData(event.target.value);
 
+   resultsWrapper.innerHTML = '';
    dropdown.classList.add('is-active');
 
    for (let movie of movies) {
       const option = document.createElement('a');
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
       option.classList.add('dropdown-item');
       option.innerHTML = `
-         <img src="${movie.Poster}" alt="${movie.Title}" title="${movie.Title}"/>
+         <img src="${imgSrc}" alt="${movie.Title}" title="${movie.Title}"/>
          ${movie.Title}
       `;
       resultsWrapper.appendChild(option);
@@ -50,3 +52,9 @@ const onInput = async event => {
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+   if (!root.contains(event.target)) {
+      dropdown.classList.remove('is-active');
+   }
+});
