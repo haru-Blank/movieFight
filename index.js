f4d24f9ed78f15bb6d51e96b533a72298935d080
harendraBhujel <avash.nekros274@gmail.com>
2020-04-01 10:08:43 +0545

    very basic search widget was implemented

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hi there!');
+const fetchData = async searchTerm => {
+   const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+         apikey: 'b1b9a789',
+         s: searchTerm
+      }
+   });
+   console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+let timeoutId;
+
+const onInput = event => {
+   /* 
+   $ Debouncing an input
+   - waiting for some time to pass after the last event to actually do something
+   */
+   if (timeoutId) {
+      clearTimeout(timeoutId);
+   }
+   timeoutId = setTimeout(() => {
+      fetchData(event.target.value);
+   }, 500);
+};
+
+input.addEventListener('input', onInput);
