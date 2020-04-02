dcabfdb35751e6c9aa27a70310a9f97f1fdad1a7
harendraBhujel <avash.nekros274@gmail.com>
2020-04-02 14:47:50 +0545

    extracted rendering logic of autocomplete

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root }) => {
+const createAutoComplete = ({ root, renderOption }) => {
    root.innerHTML = `
    <label>
       <b>Search for a Movie</b>
@@ -30,12 +30,9 @@ const createAutoComplete = ({ root }) => {
 
       for (let movie of movies) {
          const option = document.createElement('a');
-         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
          option.classList.add('dropdown-item');
-         option.innerHTML = `
-         <img src="${imgSrc}" alt="${movie.Title}" title="${movie.Title}"/>
-         ${movie.Title}
-      `;
+         option.innerHTML = renderOption(movie);
          option.addEventListener('click', () => {
             dropdown.classList.remove('is-active');
             input.value = movie.Title;
--- index.js
+++ index.js
@@ -15,15 +15,16 @@ const fetchData = async searchTerm => {
 };
 
 createAutoComplete({
-   root: document.querySelector('.autocomplete')
-});
-
-createAutoComplete({
-   root: document.querySelector('.autocomplete-two')
-});
-
-createAutoComplete({
-   root: document.querySelector('.autocomplete-three')
+   root: document.querySelector('.autocomplete'),
+   renderOption(movie) {
+      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+      return `
+         <img src="${imgSrc}" 
+         alt="${movie.Title}" 
+         title="${movie.Title}"/>
+         ${movie.Title} (${movie.Year})
+      `;
+   }
 });
 
 const onMovieSelect = async movie => {
