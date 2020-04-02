eb359a850e86c8b0f5fc6714a1a02312cd669efa
harendraBhujel <avash.nekros274@gmail.com>
2020-04-02 14:58:50 +0545

    made autocomplete totally reusabe | removed movie reference

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const fetchData = async searchTerm => {
-   const response = await axios.get('http://www.omdbapi.com/', {
-      params: {
-         apikey: 'b1b9a789',
-         s: searchTerm
-      }
-   });
-
-   if (response.data.Error) {
-      console.log(response.data.Errora);
-      return [];
-   }
-
-   return response.data.Search;
-};
-
 createAutoComplete({
    root: document.querySelector('.autocomplete'),
    renderOption(movie) {
@@ -32,6 +16,22 @@ createAutoComplete({
 
    inputValue(movie) {
       return movie.Title;
+   },
+
+   async fetchData(searchTerm) {
+      const response = await axios.get('http://www.omdbapi.com/', {
+         params: {
+            apikey: 'b1b9a789',
+            s: searchTerm
+         }
+      });
+
+      if (response.data.Error) {
+         console.log(response.data.Errora);
+         return [];
+      }
+
+      return response.data.Search;
    }
 });
 
